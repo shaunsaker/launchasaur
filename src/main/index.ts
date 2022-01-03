160886ca6844767481a8dcd819b4c10d0b20f194
Shaun Saker <sakershaun@gmail.com>
2022-01-03 09:24:13 +0200

    feature/deploy release builds to github

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,4 +1,6 @@
 import { app, BrowserWindow } from "electron";
+import initUpdateElectronApp from "update-electron-app";
+import logger from "electron-log";
 import { isDevelopment } from "../utils/isDevelopment";
 import { enableAutoLaunch } from "./autoLaunch";
 import { startIPC } from "./ipc";
@@ -13,6 +15,8 @@ if (require("electron-squirrel-startup")) {
 
 const start = (): void => {
   require("../sentry");
+
+  initUpdateElectronApp({ logger, repo: "shaunsaker/launchasaur-releases" });
 
   // Create the browser window.
   const mainWindow = new BrowserWindow({
