160326288a2e97b7a8f01f733018e919dc771103
Shaun Saker <sakershaun@gmail.com>
2022-01-05 09:14:32 +0200

    fix/attempt to fix missing fonts

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,3 +1,5 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 const rules = require("./webpack.rules");
 const plugins = require("./webpack.plugins");
 
@@ -12,12 +14,24 @@ rules.push({
     {
       loader: "file-loader",
       options: {
-        name: "[name].[ext]",
-        outputPath: "fonts/",
+        name: "[path][name].[ext]",
+        publicPath: "..", // move up from 'main_window'
+        context: "src", // set relative working folder to src
       },
     },
   ],
 });
+
+plugins.push(
+  new CopyWebpackPlugin({
+    patterns: [
+      {
+        from: path.join("src", "renderer", "fonts"),
+        to: path.join("renderer", "fonts"),
+      },
+    ],
+  }),
+);
 
 module.exports = {
   module: {
