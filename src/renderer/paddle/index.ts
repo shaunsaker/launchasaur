e14cfb7ab274730f0e2255d8d26e387f88f19e83
Shaun Saker <sakershaun@gmail.com>
2022-01-06 08:56:30 +0200

    feature/remove paddle

--- src/renderer/paddle/index.ts
+++
@@ -1,7 +0,0 @@
-import { isDevelopment } from "../../utils/isDevelopment";
-
-const Paddle = window.Paddle;
-
-Paddle.Setup({ vendor: process.env.PADDLE_VENDOR_ID, debug: isDevelopment() });
-
-export { Paddle };
