dcc33c86d17533ee5705fded49ccc80b9cb69f95
Shaun Saker <sakershaun@gmail.com>
2022-01-07 08:59:00 +0200

    fix/load env vars

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,5 +1,15 @@
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const webpack = require("webpack");
+const path = require("path");
+
+const SERVICE = process.env.SERVICE || "development";
+
+const dotenv = require("dotenv").config({
+  path: path.join(
+    __dirname,
+    SERVICE === "production" ? ".env.prod" : ".env.dev",
+  ),
+});
 
 const NODE_ENV = process.env.NODE_ENV || "development";
 
