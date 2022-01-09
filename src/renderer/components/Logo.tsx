6d9d16fd286c3370fb7c6a379e72fcd67cd2c493
Shaun Saker <sakershaun@gmail.com>
2022-01-09 08:41:05 +0200

    feature/use logo in HeaderBar

--- src/renderer/components/Logo.tsx
+++ src/renderer/components/Logo.tsx
@@ -1,6 +1,14 @@
 import React, { ReactElement } from "react";
-import { Icon } from "./Icon";
+import LogoIcon from "../icons/logo.svg";
+import { Circle } from "./Circle";
+import { ICON_SIZE } from "../theme";
+
+const LOGO_SIZE = ICON_SIZE * 2;
 
 export const Logo = (): ReactElement => {
-  return <Icon icon="rocket" />;
+  return (
+    <Circle>
+      <img src={LogoIcon} style={{ width: LOGO_SIZE, height: LOGO_SIZE }} />
+    </Circle>
+  );
 };
