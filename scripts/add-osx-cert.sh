e980f921a872ff63a01330b4b59eb1aa66b53746
Shaun Saker <sakershaun@gmail.com>
2022-01-30 09:42:43 +0200

    fix/use correct secret in adding mac certs action

--- scripts/add-osx-cert.sh
+++ scripts/add-osx-cert.sh
@@ -3,6 +3,10 @@
 KEY_CHAIN=build.keychain
 CERTIFICATE_P12=certificate.p12
 
+# Check that the secrets were set correctly
+echo $CERTIFICATE_OSX_APPLICATION
+echo $CERTIFICATE_OSX_PASSWORD
+
 # Recreate the certificate from the secure environment variable
 echo $CERTIFICATE_OSX_APPLICATION | base64 --decode > $CERTIFICATE_P12
 
@@ -15,7 +19,7 @@ security default-keychain -s $KEY_CHAIN
 # Unlock the keychain
 security unlock-keychain -p actions $KEY_CHAIN
 
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $CERTIFICATE_OSX_PASSWORD -T /usr/bin/codesign;
 
 security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
 
