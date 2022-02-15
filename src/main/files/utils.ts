41bc9c9ed32ec9ba335d0db9979391eb346f412e
Shaun Saker <sakershaun@gmail.com>
2022-02-15 17:12:46 +0200

    fix/close application on MacOS

--- src/main/files/utils.ts
+++ src/main/files/utils.ts
@@ -2,6 +2,7 @@ import { app } from "electron";
 import fkill from "fkill";
 import psList from "ps-list";
 import { getFilenameFromFilepath } from "../../utils/getFilenameFromFilepath";
+import { isMacOS } from "../utils/isMacOS";
 
 // returns the applications directory for mac and linux
 export const getAppsDir = (): string =>
@@ -21,7 +22,17 @@ export const findProcess = async (
 
   const processList = await psList();
 
-  const process = processList.filter((process) => process.name === filename)[0];
+  const process = processList.filter((process) => {
+    if (isMacOS) {
+      // on MacOS, if the file is an application the name is shortened, e.g. '/Applications/Br'
+      // so we need to check the cmd to see if the filepath was called in the command
+      if (process.cmd.includes(filename)) {
+        return process;
+      }
+    }
+
+    return process.name === filename;
+  })[0];
 
   return process;
 };
